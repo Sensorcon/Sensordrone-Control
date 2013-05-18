70a07e3919b4da230a16327e82c17edcddc4f574
Magnus Borgstrom <magnub@gmail.com>
2013-05-18 22:04:15 +0200

    Cleanup and fixes throughout the app
    * Cleaned the manifest of absolute package names, clearer to read now
    * Added configChanbges=orientation to activities we do not want to
    restart upon rotation, this means that we handle orientation changes
    ourselfes if we want to
    * Cleaned activites of the holder classes since activity is no longer
    restarted upon rotation
    * Moved status/event listeners to onpause and onresume methods (if we
    are not in foreground we do not want to listen to event/status changes i
    guess?)
    *And other fixes and cleanups

--- src/com/sensorcon/sensordronecontrol/DroneApplication.java
+++ src/com/sensorcon/sensordronecontrol/DroneApplication.java
@@ -1,32 +1,28 @@
 package com.sensorcon.sensordronecontrol;
 
+import android.app.Application;
+
 import com.sensorcon.sensordrone.Drone;
 
-import android.app.Application;
-import android.content.SharedPreferences;
-
 /**
- * We will make a class that extends Application to put our Drone in,
- * that way we can use it in multiple activities (used here for the main screen and graphing).
+ * We will make a class that extends Application to put our Drone in, that way
+ * we can use it in multiple activities (used here for the main screen and
+ * graphing).
  */
 public class DroneApplication extends Application {
 	public Drone myDrone;
-	
 
-	
-	// Set some streaming rates, so we can switch back to a default rate when coming back from 
-	// graphing.
+	// Set some streaming rates, so we can switch back to a default rate when
+	// coming back from graphing.
 	public int defaultRate = 1000;
 	public int streamingRate;
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
 		myDrone = new Drone();
 		streamingRate = defaultRate;
-				
 	}
-	
-	
+
 }
